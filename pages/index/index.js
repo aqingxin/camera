8f0de9d7ff46999f20e5fdb2bd3e1b5177c0a1d8
aqingxin <1607440679@qq.com>
2019-01-09 20:28:24 +0800

    完成选择图片，及对头像贴纸的选择、取消选择、旋转、水平翻转操作

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,25 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+const ctx = wx.createCanvasContext('firstCanvas', this);
 Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    imageUrl:'',
+    imgW:'',
+    imgH:'',
+    cW:'',
+    cH:'',
+    newW:'',
+    newH:'',
+    imgT:'',
+    boxW:50,
+    boxH:50,
+    targetX:'',
+    targetY:''
   },
   //事件处理函数
   bindViewTap: function() {
@@ -51,14 +63,106 @@ Page({
       hasUserInfo: true
     })
   },
-  toMian(){
+  imgLoad(e){
+    console.log(e)
+  },
+  selectImg:function(){
+    // var query=wx.createSelectorQuery();
+    // wx.chooseImage({
+    //   count:1,
+    //   sourceType:['album','cameta'],
+    //   success:(res)=>{
+    //     this.setData({
+    //       imageUrl:res.tempFilePaths[0]
+    //     })
+    //     var _this=this;
+    //     setTimeout(()=>{
+    //       query.select('.picture').boundingClientRect((e)=>{
+    //         console.log(e.width,e.height,e)
+    //         _this.setData({
+    //           imgH:e.height,
+    //           imgW:e.width
+    //         })
+    //       }).exec();
+
+    //       query.select('.canvas').boundingClientRect((e) => {
+    //         console.log(e.width, e.height, e)
+    //         _this.setData({
+    //           cH: e.height,
+    //           cW: e.width
+    //         })
+    //       }).exec();
+
+    //     },100)
+    //     console.log(res)
+    //     setTimeout(()=>{
+    //       var canvasY=(this.data.cH-this.data.imgH*this.data.cW/this.data.imgW)/2;
+    //       this.setData({
+    //         newW:this.data.cW,
+    //         newH: this.data.imgH * this.data.cW / this.data.imgW,
+    //         imgT:canvasY
+    //       })
+    //       ctx.drawImage(res.tempFilePaths[0], 0, canvasY, this.data.cW,this.data.imgH*this.data.cW/this.data.imgW);
+    //       ctx.draw();
+          
+    //       // })
+    //     },200)
+    //   }
+    // })
     wx.chooseImage({
+      count:1,
       success: function(res) {
-        // console.log(res.tempFilePaths[0]);
+        console.log(res.tempFilePaths[0])
         wx.navigateTo({
-          url: `../main/main?img=${res.tempFilePaths[0]}`,
+          url: '../main/main?img='+res.tempFilePaths[0],
         })
       },
     })
+  },
+  boxStart(e){
+    // console.log(e)
+    this.setData({
+      targetX:e.touches[0].clientX,
+      targetY: e.touches[0].clientY
+    })
+    // targetX: '',
+    // targetY: ''
+  },
+  saveImg(){
+    wx.canvasToTempFilePath({
+      x: 0,
+      y: 0,
+      width: 375,
+      height: 667,
+      destWidth: 375 * 2,
+      destHeight: 667 * 2,
+      canvasId: 'firstCanvas',
+      success: res => {
+        console.log('data:');
+        console.log(res)
+        wx.saveImageToPhotosAlbum({
+          filePath: res.tempFilePath,
+        })
+      }
+    })
+  },
+  boxChange(e){
+    var parseTop='';
+    var parseLeft='';
+    var parseW='';
+    var parseH='';
+    wx.createSelectorQuery().select('.box').boundingClientRect().exec(function(ev){
+      parseLeft=ev[0].left;
+      parseTop = ev[0].top;
+      parseW = ev[0].width;
+      parseH = ev[0].height;
+      // console.log(ev)
+    })
+    
+    // setTimeout(()=>{
+      this.setData({
+        boxW: e.touches[0].clientX - this.data.targetX + parseW,
+        boxH: e.touches[0].clientY - this.data.targetY + parseH,
+      })
   }
 })
--- pages/main/main.js
+++ pages/main/main.js
@@ -1,42 +1,134 @@
 // pages/main/main.js
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    imgSrc:'',
-    imgW:0,
-    imgH:0
+    tmpImg:'',
+    imgWidth:'',
+    imgHeight:'',
+    originalWidth:'',   //图片真实宽度
+    originalHeight:'',   //图片真实高度
+    arrowPosition:'0%',
+    arrowRotate:'0deg',
+    imgArr:[
+      { src: '1.png', rotate: 0, flip: '0deg', scale: '1' },
+      { src: '2.png', rotate: 0, flip: '0deg', scale: '1' },
+      { src: '3.png', rotate: 0, flip: '0deg', scale: '1' },
+    ],
+    choseImg:[],
+    imgIndex:null,
+    startX: null,
+    startY: null,
+    moveX: null,
+    moveY: null,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // console.log(options)
-    this.setData({
-      imgSrc:options.img
+    this.setData({    //设置图片路径
+      tmpImg:options.img
     })
   },
-  imageLoad:function(e) {
-    // console.log(e)
-    // this.setData({
-    //   imgW:e.detail.width,
-    //   imgH:e.detail.height
-    // })
-    let tmpW = e.detail.width;
-    let tmpH = e.detail.height;
-    var query=wx.createSelectorQuery();
-    query.select('.main-content').boundingClientRect(rect=>{
-      // console.log(rect)
-      this.setData({
-        imgW:rect.width,
-        imgH:tmpH*rect.width/tmpW
-      })
-    }).exec();
-  },
 
+  mainImgLoad(e){    //图片加载完执行该函数
+    var query=wx.createSelectorQuery();
+    var tmpWidth = e.detail.width;
+    var tmpHeight = e.detail.height;
+    var _this=this;
+    query.select('.main-content').boundingClientRect().exec(function(ev){
+      // console.log(ev)
+      _this.setData({
+        originalHeight: e.detail.height,    //设置图片原大小
+        originalWidth: e.detail.width,
+        imgWidth:ev[0].width,      //设置图片的显示大小，使其自适应
+        imgHeight: tmpHeight * ev[0].width/tmpWidth
+      })
+    })
+  },
+  changeArrow(){  //贴纸素材容器的样式
+    if (this.data.arrowPosition==='0%'){
+      this.setData({
+        arrowPosition:'80%',
+        arrowRotate:'180deg'
+      })
+    }else{
+      this.setData({
+        arrowPosition: '0%',
+        arrowRotate: '0deg'
+      })
+    }
+  },
+  selectImg(e){  //从素材中选择贴纸，在显示到主要区域中
+    console.log(e)
+    let tmpArr = this.data.choseImg;
+    let tmpImg = JSON.parse(JSON.stringify(this.data.imgArr));   //深拷贝的一种技巧，防止修改数据时把源数据也一起修改了
+    tmpArr.push(tmpImg[e.target.dataset.index]);
+    this.setData({
+      choseImg: tmpArr,
+    })
+  },
+  choseImgLoad(e){
+    // console.log(e)
+    this.setData({
+      imgIndex: e.target.dataset.index
+    })
+  },
+  closeChoseImg() {   //删除已选择的头像贴纸
+    let tmpArr=this.data.choseImg;
+    tmpArr.splice(this.data.imgIndex,1);
+    this.setData({
+      choseImg:tmpArr,
+      imgIndex: this.data.imgIndex-1
+    })
+  },
+  flipChoseImg(){    //水平翻转头像贴纸
+    var query = wx.createSelectorQuery();
+    var _this=this;
+    query.select(`#choseImg${this.data.imgIndex}`).boundingClientRect().exec(function(ev){
+      // console.log(ev[0].dataset.index)
+      let tmp = _this.data.choseImg;
+      if (tmp[ev[0].dataset.index].flip === '180deg'){
+        tmp[ev[0].dataset.index].flip = '0deg';
+      }else{
+        tmp[ev[0].dataset.index].flip = '180deg'
+      }
+      _this.setData({
+        choseImg:tmp
+      })
+    })
+  },
+  rotateChoseImg() {
+
+  },
+  rotateStart(e) {  
+    console.log(e)
+    this.setData({
+      startX: e.changedTouches[0].pageX,
+      startY: e.changedTouches[0].pageY,
+    })
+  },
+  rotateImgMove(e) {   //旋转时手指的滑动
+    var query = wx.createSelectorQuery();
+    var _this = this;
+    let pageX=e.changedTouches[0].pageX;
+    let pageY = e.changedTouches[0].pageY;
+    query.select(`#choseImg${this.data.imgIndex}`).boundingClientRect().exec(function (ev) {
+      // console.log(ev)
+      
+      let tmp = _this.data.choseImg;
+      tmp[ev[0].dataset.index].rotate = Math.atan2(pageY - _this.data.startY, pageX  - _this.data.startX  ) / Math.PI * 180
+      _this.setData({
+        choseImg: tmp,
+        moveX:pageX,
+        moveY:pageY
+      })
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
